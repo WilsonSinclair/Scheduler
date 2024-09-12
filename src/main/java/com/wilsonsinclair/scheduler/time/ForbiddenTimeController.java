8cf17de8959e89cf7f68de121485daf903796394
WilsonSinclair <wilsonsinclair6@gmail.com>
2024-09-11 23:14:00 -0500

    Adding a Forbidden Time of a certain time variety now correctly enables the correct combo boxes and those combo boxes are populated with their correct values

--- src/main/java/com/wilsonsinclair/scheduler/time/ForbiddenTimeController.java
+++ src/main/java/com/wilsonsinclair/scheduler/time/ForbiddenTimeController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.DatePicker;
 import java.net.URL;
 import java.time.DayOfWeek;
 import java.util.ResourceBundle;
+import java.util.stream.IntStream;
 
 public class ForbiddenTimeController implements Initializable {
 
@@ -25,11 +26,14 @@ public class ForbiddenTimeController implements Initializable {
     private ComboBox<DayOfWeek> dayOFWeekComboBox;
 
     @FXML
-    private ComboBox<Integer> startTimeComboBox, endTimeComboBox;
+    private ComboBox<Integer> startTimeComboBox, endTimeComboBox, startMinuteComboBox, endMinuteComboBox;
 
     private ForbiddenTime forbiddenTime;
 
     private final String[] CHOICE_BOX_VALUES = {"Certain Date", "Certain Time", "Day of Week"};
+
+    private final Integer[] HOURS = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    private final int[] MINUTES = IntStream.rangeClosed(0, 59).toArray();
 
     public ForbiddenTime getForbiddenTime() { return forbiddenTime; }
 
@@ -69,6 +73,14 @@ public class ForbiddenTimeController implements Initializable {
         dayOFWeekComboBox.setOnAction(comboBoxEvent);
         datePicker.setOnAction(datePickerEvent);
 
+        startTimeComboBox.setItems(FXCollections.observableArrayList(HOURS));
+        endTimeComboBox.setItems(FXCollections.observableArrayList(HOURS));
+
+        // what the fuck
+        Integer[] minutes = IntStream.of(MINUTES).boxed().toArray(Integer[]::new);
+        startMinuteComboBox.setItems(FXCollections.observableArrayList(minutes));
+        endMinuteComboBox.setItems(FXCollections.observableArrayList(minutes));
+
         //Listen for changes to this choice box
         choiceBox.getSelectionModel().selectedIndexProperty().addListener((observableValue, value, newValue) -> {
             switch (CHOICE_BOX_VALUES[newValue.intValue()]) {
@@ -79,6 +91,10 @@ public class ForbiddenTimeController implements Initializable {
                 case "Certain Time":
                     datePicker.setDisable(true);
                     dayOFWeekComboBox.setDisable(true);
+                    startTimeComboBox.setDisable(false);
+                    endTimeComboBox.setDisable(false);
+                    startMinuteComboBox.setDisable(false);
+                    endMinuteComboBox.setDisable(false);
                     break;
                 case "Day of Week":
                     dayOFWeekComboBox.setDisable(false);
