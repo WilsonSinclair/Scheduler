ee68e69fd7fc5994d0d68ff5318f4dd4a927c787
WilsonSinclair <wilsonsinclair6@gmail.com>
2025-04-16 19:06:41 -0500

    Fixed a test failing because a new line issue.

--- src/test/java/com/wilsonsinclair/scheduler/time/ShiftTest.java
+++ src/test/java/com/wilsonsinclair/scheduler/time/ShiftTest.java
@@ -22,6 +22,6 @@ public class ShiftTest {
     void toStringTest() {
         Employee e = getRandomEmployee();
         Shift shift = new Shift(e, LocalTime.of(9, 0), LocalTime.of(12, 0));
-        assertEquals(shift.toString(), e.getName() + "\nIn: 09:00\nOut: 12:00");
+        assertEquals(shift.toString(), e.getName() + "\nIn: 09:00\nOut: 12:00\n");
     }
 }
